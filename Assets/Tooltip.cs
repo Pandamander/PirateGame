34a92c6b42d78a1232b85c62afeb331a167dfe27
bromoco <brice.morrison@gmail.com>
2022-02-25 09:52:22 -0800

    Adding a tooltip for repareparing when you get close to the leak. Coop feel free to edit the look of this

---
+++ Assets/Tooltip.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tooltip : MonoBehaviour
+{
+    [SerializeField] BoxCollider2D collider;
+    [SerializeField] GameObject toolTip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // hide the tooltip to start
+        toolTip.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+     // make the tooltip appear and disappears when player is here
+        if (collider.gameObject.GetComponent<Player>())
+        {
+            Debug.Log("Trigger enter player");
+            toolTip.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        // make the tooltip disappear
+        if (collider.gameObject.GetComponent<Player>())
+        {
+            Debug.Log("Trigger exit player");
+            toolTip.SetActive(false);
+        }
+    }
+}
