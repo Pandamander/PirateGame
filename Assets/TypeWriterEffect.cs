c7bd896afbcf3a2fc68afc04d15cc7db98c94429
bromoco <brice.morrison@gmail.com>
2022-02-28 00:18:31 -0800

    Added two key presses to read story

--- Assets/TypeWriterEffect.cs
+++ Assets/TypeWriterEffect.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TypeWriterEffect : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class TypeWriterEffect : MonoBehaviour
     public string fullText;
     public bool textCompleted = false;
     private string currentText;
+    private int i;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +22,7 @@ public class TypeWriterEffect : MonoBehaviour
 
     IEnumerator ShowText()
     {
-        for (int i = 0; i < fullText.Length + 1; i++)
+        for (i = 0; i < fullText.Length + 1; i++)
         {
             currentText = fullText.Substring(0, i);
             this.GetComponent<TMP_Text>().text = currentText;
@@ -30,5 +32,27 @@ public class TypeWriterEffect : MonoBehaviour
             }
             yield return new WaitForSeconds(delaySpeed);
         }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && textCompleted) // Custom code to advance scene
+        {
+            SceneManager.LoadScene("SampleScene");
+
+        }
+
+
+        if (Input.anyKey)
+        {
+                currentText = fullText;
+                this.GetComponent<TMP_Text>().text = currentText;
+                i = fullText.Length;
+                textCompleted = true;
+            
+           
+        }
+
+       
     }
 }
